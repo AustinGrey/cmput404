ca61c8eafbaa9090d3b8b807de4505f57cb36e86
Austin Crapo <crapo@cs.ualberta.ca>
2020-01-07 08:36:08 -0700

    Updated lab1 for W20 semester

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+# This script was originally submitted when I looked at this course over a year ago - I have modified it to match the current version of the lab and resubmitted for this semester.
 
 import requests
 #print(requests.__version__)
 
 #input()
 
-#print(requests.get("http://www.google.com/teapot"))
+#google = requests.get("http://www.google.com")
+#print(google)
+
+
 print(requests.get("https://raw.githubusercontent.com/AustinGrey/cmput404/master/lab1.py").text)
